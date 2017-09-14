5ab522d8104977f13c3be5d9309486d6530c3605
kkumar30 <kkumarshivi@gmail.com>
2017-09-14 00:43:18 -0400

    Setting up OpenCV distance calculator

---
+++ OpenCV/test.py
@@ -0,0 +1,49 @@
+"""
+wewe
+""""
+
+
+import numpy  # requires the numpy+mkl pkg
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+def midpoint(point1, point2):
+	return (point1[0] + point2[0]) * 0.5, (point1[1] + point2[1]) * 0.5
+
+
+
+
+def find_marker(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert image to grayscale
+    blur = cv2.cvtColor(gray, (5,5), 0)
+    edged = cv2.Canny(blur, 35, 125)
+    # Find the contours in the edged image and keep the largest one;
+    # we'll assume that this is our piece of paper in the image
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    c = max(cnts, key=cv2.contourArea)
+    return cv2.minAreaRect(c)
+
+
+# compute and return the distance from the maker to the camera
+def distance_to_camera(knownWidth, focalLength, perWidth):
+    return (knownWidth * focalLength) / perWidth
+
+# initialize the known distance from the camera to the object, which in this case is 24 inches
+KNOWN_DISTANCE = 24.0
+# initialize the known object width, which in this case, the piece of paper is 11 inches wide
+KNOWN_WIDTH = 11.0
+
+# initialize the list of images that we'll be using
+IMAGE_PATHS = ["*.png", "*.png", "*.png"]
+
+# load the furst image that contains an object that is KNOWN TO BE 2 feet
+# from our camera, then find the paper marker in the image, and initialize
+# the focal length
+image = cv2.imread(IMAGE_PATHS[0])
+marker = find_marker(image)
+focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
