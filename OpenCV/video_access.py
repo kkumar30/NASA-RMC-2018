48949ac3bf986c7af649d16637fc718a92bc35fc
kkumar30 <kkumarshivi@gmail.com>
2017-09-23 13:28:37 -0400

    fixed crashing of vid window

--- OpenCV/video_access.py
+++ OpenCV/video_access.py
@@ -1,12 +1,13 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+print cap.isOpened()
 
 while True:
     ret, frame = cap.read()
     cv2.imshow("Frame", frame)
 
-    if cv2.waitKey(1) & 0xFF:
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
