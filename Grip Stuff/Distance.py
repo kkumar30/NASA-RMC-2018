2e683c9db3a9ed91936469e6b4ee746513aa1bb1
Tom Hagen <thagen@wpi.edu>
2017-09-27 13:49:12 -0400

    working on distance

--- Grip Stuff/Distance.py
+++ Grip Stuff/Distance.py
@@ -12,8 +12,8 @@ import time
 import cv2
 #import matplotlib.pyplot as plt
 
-from Pipeline import GripPipeline
-
+# from Pipeline import GripPipeline
+from redObject import RedObjectPipeline
 
 def getDistance(pipeline):
     """
@@ -26,6 +26,12 @@ def getDistance(pipeline):
     widths = []
     heights = []
 
+    #TODO##########
+    camera_x_center =  #in px
+    camera_y_center =  #in px
+
+    ###############
+
 
     # Find the bounding boxes of the contours to get x, y, width, and height
     for contour in pipeline.filter_contours_output:
@@ -37,17 +43,36 @@ def getDistance(pipeline):
 
         print widths,heights
 
+    # TODO implement this for an array
+    #TODO implement a pxToFt function
+    #We have now found hopefully only one contour
+    #TODO find the distance from the center of the camera to the wall
 
+    # distToWall =
+
+    #With this distance we can use trig with the center of the camera to the x coordinate of the object to find the diagonal distance
+    #find the difference in X
+
+    # x_diff = convertPxToFt(abs(center_x_positions()-camera_x_center))
+    # yaw = atan(x_diff/distToWall)
+    # yaw_dist = distToWall*cos(yaw)
+
+    #Now figure out the other diagonal distance with the y coordinate of the object
+    # y_diff = convertPxToFt(abs(center_y_positions()-camera_y_center))
+    pitch = atan(y_diff/distToWall)
+    # diagonalDist = yaw_dist*cos(pitch)
+    print "Total Distance to object" diagonalDist "ft"
+    print "Horizontal Distance to object" distToWall "ft"
 
 
 
 def main():
     print('Creating video capture')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     print "framerate", cap.get(5)
 
     print('Creating pipeline')
-    pipeline = GripPipeline()
+    pipeline = RedObjectPipeline()
 
     print('Running pipeline')
     while cap.isOpened():
@@ -55,12 +80,14 @@ def main():
         if have_frame:
             pipeline.process(frame)
             img1 = frame.copy()
-            cv2.drawContours(img1, pipeline.filter_contours_output, -1, (255, 0, 0), 3)
-            cv2.imshow("contours", img1)
+
+            cv2.imshow("contours", cv2.drawContours(img1, pipeline.filter_contours_output, -1, (255, 0, 0), 3))
             getDistance(pipeline)
 
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
     print('Capture closed')
-
+    cap.release()
 
 if __name__ == '__main__':
     main()
