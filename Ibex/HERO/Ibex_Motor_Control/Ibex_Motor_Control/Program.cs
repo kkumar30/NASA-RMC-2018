73754c36845b562a7c4e2362fff9574f8068ee12
thagen15 <thagen@wpi.edu>
2018-04-08 17:15:54 -0400

    added motors to C# code

--- Ibex/HERO/Ibex_Motor_Control/Ibex_Motor_Control/Program.cs
+++ Ibex/HERO/Ibex_Motor_Control/Ibex_Motor_Control/Program.cs
@@ -19,14 +19,14 @@ namespace Ibex_Motor_Control
         private const int RESET_DRIVE_ENC = 0;
         private const int RESET_DEPTH_ENC = 1;
         private const int RESET_SCOOP_ENC = 2;
-        private const int RESET_WINCH_ENC = 3;
+        private const int RESET_CONVEYOR_ENC = 3;
 
         //Flags for Motor ID's
         private const int LDRIVE_ID = 0;
         private const int RDRIVE_ID = 1;
         private const int SCOOP_ID = 2;
         private const int DEPTH_ID = 3;
-        private const int WINCH_ID = 4;
+        private const int conveyor_ID = 4;
 
         static bool[] flags = { false, false, false, false };
 
@@ -36,110 +36,92 @@ namespace Ibex_Motor_Control
         public static void Main()
         {
             //Set up the motors
-            CTRE.TalonSrx testmotor = new CTRE.TalonSrx(1);
-            CTRE.TalonSrx testmotor1 = new CTRE.TalonSrx(2);
+            CTRE.TalonSrx leftDriveMotor = new CTRE.TalonSrx(1);
+            CTRE.TalonSrx rightDriveMotor = new CTRE.TalonSrx(2);
+            CTRE.TalonSrx scoopMotor = new CTRE.TalonSrx(3);
+            CTRE.TalonSrx depthMotor = new CTRE.TalonSrx(4);
+            CTRE.TalonSrx conveyorMotor = new CTRE.TalonSrx(5);
 
-            /*          CTRE.TalonSrx rightMotor = new CTRE.TalonSrx(2);
-                        CTRE.TalonSrx scoopMotor = new CTRE.TalonSrx(3);
-                        CTRE.TalonSrx depthMotor = new CTRE.TalonSrx(4);
-                        CTRE.TalonSrx winchMotor = new CTRE.TalonSrx(5);
-            */
-            //Set encoders for each motor            
-            testmotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
-            testmotor1.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
-            /*            
-                        rightMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
-                        scoopMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
-                        depthMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
-                        winchMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
-            */
-            //Set direction of the encoders            
-            testmotor.SetSensorDirection(false);
-            testmotor1.SetSensorDirection(false);
-            /*          rightMotor.SetSensorDirection(true);
-                        scoopMotor.SetSensorDirection(false);
-                        depthMotor.SetSensorDirection(false);
-                        winchMotor.SetSensorDirection(false);
-            */
+            //Set encoders for each motor
+            leftDriveMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
+            rightDriveMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
+            scoopMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
+            depthMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
+            conveyorMotor.SetFeedbackDevice(CTRE.TalonSrx.FeedbackDevice.QuadEncoder);
+
+            //Set direction of the encoders
+            leftDriveMotor.SetSensorDirection(false);
+            rightDriveMotor.SetSensorDirection(false);
+            scoopMotor.SetSensorDirection(false);
+            depthMotor.SetSensorDirection(false);
+            conveyorMotor.SetSensorDirection(false);
+
             //Set Ticks per Rev of the encoders
-            testmotor.ConfigEncoderCodesPerRev(80);
-            testmotor1.ConfigEncoderCodesPerRev(80);
-            /*          rightMotor.ConfigEncoderCodesPerRev(80);
-                        scoopMotor.ConfigEncoderCodesPerRev(80);
-                        depthMotor.ConfigEncoderCodesPerRev(80);
-                        winchMotor.ConfigEncoderCodesPerRev(80);
-            */
+            leftDriveMotor.ConfigEncoderCodesPerRev(80);
+            rightDriveMotor.ConfigEncoderCodesPerRev(80);
+            scoopMotor.ConfigEncoderCodesPerRev(80);
+            depthMotor.ConfigEncoderCodesPerRev(80);
+            conveyorMotor.ConfigEncoderCodesPerRev(80);
+
             //Sets PIDF values for each motor//
-            testmotor.SetP(0, 0.35F);
-            testmotor.SetI(0, 0.0F);
-            testmotor.SetD(0, 0.0F);
-            testmotor.SetF(0, 0.0F);
-            testmotor.SelectProfileSlot(0);
+            leftDriveMotor.SetP(0, 0.35F);
+            leftDriveMotor.SetI(0, 0.0F);
+            leftDriveMotor.SetD(0, 0.0F);
+            leftDriveMotor.SetF(0, 0.0F);
+            leftDriveMotor.SelectProfileSlot(0);
 
-            testmotor1.SetP(0, 0.35F);
-            testmotor1.SetI(0, 0.0F);
-            testmotor1.SetD(0, 0.0F);
-            testmotor1.SetF(0, 0.0F);
-            testmotor1.SelectProfileSlot(0);
+            rightDriveMotor.SetP(0, 0.35F);
+            rightDriveMotor.SetI(0, 0.0F);
+            rightDriveMotor.SetD(0, 0.0F);
+            rightDriveMotor.SetF(0, 0.0F);
+            rightDriveMotor.SelectProfileSlot(0);
 
-            /*            rightMotor.SetP(0, 0.35F);
-                        rightMotor.SetI(0, 0.0F);
-                        rightMotor.SetD(0, 0.0F);
-                        rightMotor.SetF(0, 0.0F);
-                        rightMotor.SelectProfileSlot(0);
 
-                        scoopMotor.SetP(0, 0.6F);
-                        scoopMotor.SetI(0, 0.0F);
-                        scoopMotor.SetD(0, 0.0F);
-                        scoopMotor.SetF(0, 0.0F);
-                        scoopMotor.SelectProfileSlot(0);
+            scoopMotor.SetP(0, 0.6F);
+            scoopMotor.SetI(0, 0.0F);
+            scoopMotor.SetD(0, 0.0F);
+            scoopMotor.SetF(0, 0.0F);
+            scoopMotor.SelectProfileSlot(0);
 
-                        depthMotor.SetP(0, 0.6F);
-                        depthMotor.SetI(0, 0.0F);
-                        depthMotor.SetD(0, 0.0F);
-                        depthMotor.SetF(0, 0.0F);
-                        depthMotor.SelectProfileSlot(0);
+            depthMotor.SetP(0, 0.6F);
+            depthMotor.SetI(0, 0.0F);
+            depthMotor.SetD(0, 0.0F);
+            depthMotor.SetF(0, 0.0F);
+            depthMotor.SelectProfileSlot(0);
 
-                        winchMotor.SetP(0, 0.6F);
-                        winchMotor.SetI(0, 0.0F);
-                        winchMotor.SetD(0, 0.0F);
-                        winchMotor.SetF(0, 0.0F);
-                        winchMotor.SelectProfileSlot(0);
-                        //////////////////////////////////
-             */
+            conveyorMotor.SetP(0, 0.6F);
+            conveyorMotor.SetI(0, 0.0F);
+            conveyorMotor.SetD(0, 0.0F);
+            conveyorMotor.SetF(0, 0.0F);
+            conveyorMotor.SelectProfileSlot(0);
 
             //Sets Nominal Output Voltage for each motor
-            testmotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
-            testmotor1.ConfigNominalOutputVoltage(+0.0F, -0.0F);
-            /*rightMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
+            leftDriveMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
+            rightDriveMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
             scoopMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
             depthMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
-            winchMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
-*/
+            conveyorMotor.ConfigNominalOutputVoltage(+0.0F, -0.0F);
+
             // Set allowed error for closed loop feedback
-            testmotor.SetAllowableClosedLoopErr(0, 0);
-            testmotor1.SetAllowableClosedLoopErr(0, 0);
-            /*rightMotor.SetAllowableClosedLoopErr(0, 0);
+            leftDriveMotor.SetAllowableClosedLoopErr(0, 0);
+            rightDriveMotor.SetAllowableClosedLoopErr(0, 0);
             scoopMotor.SetAllowableClosedLoopErr(0, 0);
             depthMotor.SetAllowableClosedLoopErr(0, 0);
-            winchMotor.SetAllowableClosedLoopErr(0, 0);
-            */
+            conveyorMotor.SetAllowableClosedLoopErr(0, 0);
+
             //Set Initial position of the motors
-            testmotor.SetPosition(0);
-            testmotor1.SetPosition(0);
-            /* rightMotor.SetPosition(0);
+            leftDriveMotor.SetPosition(0);
+            rightDriveMotor.SetPosition(0);
              scoopMotor.SetPosition(0);
              depthMotor.SetPosition(0);
-             winchMotor.SetPosition(0);
-             */
+             conveyorMotor.SetPosition(0);
+
             //Sets Voltage Ramp rate of each motor
-            testmotor.SetVoltageRampRate(0);
-            testmotor1.SetVoltageRampRate(0);
-            /*rightMotor.SetVoltageRampRate(0);
+            leftDriveMotor.SetVoltageRampRate(0);
+            rightDriveMotor.SetVoltageRampRate(0);
             scoopMotor.SetVoltageRampRate(0);
             depthMotor.SetVoltageRampRate(0);
-            winchMotor.SetVoltageRampRate(0);
-            */
+            conveyorMotor.SetVoltageRampRate(0);
 
 
             ArrayList motorSetpointData = new ArrayList();
@@ -147,45 +129,40 @@ namespace Ibex_Motor_Control
             ArrayList talons = new ArrayList();
 
             //Add talons to each motor
-            talons.Add(testmotor);
-            talons.Add(testmotor1);
-            /*talons.Add(rightMotor);
+            talons.Add(leftDriveMotor);
+            talons.Add(rightDriveMotor);
             talons.Add(scoopMotor);
             talons.Add(depthMotor);
-            talons.Add(winchMotor);
-            */
-            //Initializes inbound and outbound message strings to empty            
+            talons.Add(conveyorMotor);
+
+            //Initializes inbound and outbound message strings to empty
             String inboundMessageStr = "";
             String outboundMessageStr = "";
 
             //Initializes and adds the SetpointData and the StatusData for each motor (ID, mode, setpoint//
-            SetpointData testmotorSetpointData = new SetpointData(1, 0, 0.0F);
-            SetpointData testmotor1SetpointData = new SetpointData(2, 0, 0.0F);
-            /*SetpointData rightMotorSetpointData = new SetpointData(2, 0, 0.0F);
+            SetpointData leftDriveMotorSetpointData = new SetpointData(1, 0, 0.0F);
+            SetpointData rightDriveMotorSetpointData = new SetpointData(2, 0, 0.0F);
             SetpointData scoopMotorSetpointData = new SetpointData(3, 0, 0.0F);
             SetpointData depthMotorSetpointData = new SetpointData(4, 0, 0.0F);
-            SetpointData winchMotorSetpointData = new SetpointData(5, 0, 0.0F);
-*/
-            StatusData testmotorStatusData = new StatusData(1, testmotor);
-            StatusData testmotor1StatusData = new StatusData(2, testmotor1);
-            /*StatusData rightMotorStatusData = new StatusData(2, rightMotor);
+            SetpointData conveyorMotorSetpointData = new SetpointData(5, 0, 0.0F);
+
+            StatusData leftDriveMotorStatusData = new StatusData(1, leftDriveMotor);
+            StatusData rightDriveMotorStatusData = new StatusData(2, rightDriveMotor);
             StatusData scoopMotorStatusData = new StatusData(3, scoopMotor);
             StatusData depthMotorStatusData = new StatusData(4, depthMotor);
-            StatusData winchMotorStatusData = new StatusData(5, winchMotor);
-*/
-            motorSetpointData.Add(testmotorSetpointData);
-            motorSetpointData.Add(testmotor1SetpointData);
-            /*motorSetpointData.Add(rightMotorSetpointData);
+            StatusData conveyorMotorStatusData = new StatusData(5, conveyorMotor);
+
+            motorSetpointData.Add(leftDriveMotorSetpointData);
+            motorSetpointData.Add(rightDriveMotorSetpointData);
             motorSetpointData.Add(scoopMotorSetpointData);
             motorSetpointData.Add(depthMotorSetpointData);
-            motorSetpointData.Add(winchMotorSetpointData);
-*/
-            motorStatusData.Add(testmotorStatusData);
-            motorStatusData.Add(testmotor1StatusData);
-            /*motorStatusData.Add(rightMotorStatusData);
+            motorSetpointData.Add(conveyorMotorSetpointData);
+
+            motorStatusData.Add(leftDriveMotorStatusData);
+            motorStatusData.Add(rightDriveMotorStatusData);
             motorStatusData.Add(scoopMotorStatusData);
             motorStatusData.Add(depthMotorStatusData);
-            motorStatusData.Add(winchMotorStatusData);*/
+            motorStatusData.Add(conveyorMotorStatusData);*/
             //////////////////////////////////////////
 
             CTRE.Watchdog.Feed();
@@ -206,8 +183,8 @@ namespace Ibex_Motor_Control
                 if (motorSetpointData.Count > 0)
                 {
                     //if any of the talon positions need to be reset, this will reset them
-                    //resetEncoderPositions(talons);
-                    //CTRE.Watchdog.Feed();
+                    resetEncoderPositions(talons);
+                    CTRE.Watchdog.Feed();
 
                     //attempt to process whatever was contained in the most recent message
                     processInboundData(motorSetpointData, talons);
@@ -222,24 +199,17 @@ namespace Ibex_Motor_Control
                 Debug.Print(outboundMessageStr);
                 CTRE.Watchdog.Feed();
 
-                /*string[] teststring = outboundMessageStr.Split(':');
-                Debug.Print(" Enc:" + teststring[5] + " Enc2:"+ teststring[14]);
-                CTRE.Watchdog.Feed();*/
-
                 //send that message back to the main CPU
                 Debug.Print(outboundMessageStr);
-                //if (uart.IsOpen) { 
+                //if (uart.IsOpen) {
                 writeUART(outboundMessageStr);
                 CTRE.Watchdog.Feed();
                 //}
-                CTRE.Watchdog.Feed();
 
-                //Debug.Print(testmotor.GetPosition().ToString());
-                //CTRE.Watchdog.Feed();
 
             }
         }
-        
+
         private static ArrayList readUART(ref String messageStr)
         {
             ArrayList setpointData = new ArrayList();
@@ -248,7 +218,7 @@ namespace Ibex_Motor_Control
                 //Debug.Print("Here");
                 int readCnt = uart.Read(rx, 0, 1024);
                 for (int i = 0; i < readCnt; ++i)
-                {   
+                {
                     messageStr += (char)rx[i];
                     if ((char)rx[i] == '\n')
                     {
@@ -268,23 +238,23 @@ namespace Ibex_Motor_Control
             //Debug.Print("Value = ");
             //Debug.Print("Value = " + setpointData[0]);
 
-            if (setpointData.Count > 1)
-            {
-                
-                SetpointData testsetdata = (SetpointData)setpointData[0];
-                //Debug.Print("Value: " + testsetdata.getSetpoint() + "ID: " + testsetdata.getDeviceID());
-                //Debug.Print("Value: " + testsetdata.getSetpoint() + " Device ID: " + testsetdata.getDeviceID());
-
-                SetpointData testsetdata1 = (SetpointData)setpointData[1];
-                //Debug.Print("Value: " + testsetdata1.getSetpoint() + " Device ID: " + testsetdata1.getDeviceID());
-                CTRE.Watchdog.Feed();
-            }
+            // if (setpointData.Count > 1)
+            // {
+            //
+            //     SetpointData testsetdata = (SetpointData)setpointData[0];
+            //     //Debug.Print("Value: " + testsetdata.getSetpoint() + "ID: " + testsetdata.getDeviceID());
+            //     //Debug.Print("Value: " + testsetdata.getSetpoint() + " Device ID: " + testsetdata.getDeviceID());
+            //
+            //     SetpointData testsetdata1 = (SetpointData)setpointData[1];
+            //     //Debug.Print("Value: " + testsetdata1.getSetpoint() + " Device ID: " + testsetdata1.getDeviceID());
+            //     CTRE.Watchdog.Feed();
+            // }
             return setpointData;
         }
 
         private static void resetEncoderPositions(ArrayList talons)
         {
-            //If any flags for encoder reset are true, reset the encoder 
+            //If any flags for encoder reset are true, reset the encoder
             if (flags[RESET_DRIVE_ENC])
             {
                 flags[RESET_DRIVE_ENC] = false;
@@ -314,14 +284,9 @@ namespace Ibex_Motor_Control
                 flags[RESET_SCOOP_ENC] = false;
                 ((CTRE.TalonSrx)talons[SCOOP_ID]).SetPosition(0.0F);
             }
-            if (flags[RESET_WINCH_ENC])
+            if (flags[RESET_CONVEYOR_ENC])
             {
-                flags[RESET_WINCH_ENC] = false;
-                ((CTRE.TalonSrx)talons[WINCH_ID]).SetPosition(0.0F);
-            }
-        }
-
-        private static void checkEncoderResetFlags(String msg)
+                flags[RESET_CONVEYOR_msg)
         {
             //Check the message to see if any encoders need to be reset
 
@@ -337,9 +302,9 @@ namespace Ibex_Motor_Control
             {
                 flags[RESET_DEPTH_ENC] = true;
             }
-            if (msg.Trim().Equals("<ResetWinchEncoder>"))
+            if (msg.Trim().Equals("<ResetConveyorEncoder>"))
             {
-                flags[RESET_WINCH_ENC] = true;
+                flags[RESET_CONVEYOR_ENC] = true;
             }
         }
 
@@ -369,7 +334,7 @@ namespace Ibex_Motor_Control
             return retval;
         }
 
-        //Takes in an arraylist of statusData and updates each motor's status based on the arraylist        
+        //Takes in an arraylist of statusData and updates each motor's status based on the arraylist
         private static void updateMotorStatusData(ArrayList statusData)
         {
             for (int i = 0; i < statusData.Count; i++)
@@ -379,7 +344,7 @@ namespace Ibex_Motor_Control
             }
         }
 
-        //Goes through each motors' status data and creates a string with each status data 
+        //Goes through each motors' status data and creates a string with each status data
         private static String makeOutboundMessage(ArrayList statusData)
         {
             String outboundMessage = "";
@@ -392,17 +357,16 @@ namespace Ibex_Motor_Control
             return outboundMessage;
         }
 
-        //I think this is where the magic happens and the motors move
         private static void processInboundData(ArrayList setpointDataList, ArrayList talons)
         {
-            //For each setpointData, for the talon that matches it set each mode and setpoint based on the list information 
+            //For each setpointData, for the talon that matches it set each mode and setpoint based on the list information
             for (int i = 0; i < setpointDataList.Count; i++)
             {
                 try
                 {
                 SetpointData setpointData = (SetpointData)setpointDataList[i];
                 float setpointVal = (float)(setpointData.getSetpoint());
-                
+
                     CTRE.TalonSrx talon = (CTRE.TalonSrx)talons[i];
                     if (talon.GetControlMode() != setpointData.getMode())
                     {
